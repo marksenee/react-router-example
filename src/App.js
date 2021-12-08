0b8175a143cbcf613e20a390f91c60fabcbbd746
markseeun <mark233891@gmaiil.com>
2021-12-08 11:41:29 +0900

    NavLink component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
 import Links from './components/Links';
+import NavLinks from './components/NavLinks';
 
 function App() {
   return (
       <BrowserRouter>
       <Links />
+      <NavLinks />
       <Switch>
           <Route path="/profile/:id" component={Profile}></Route>
           <Route path="/profile" component={Profile}></Route>
